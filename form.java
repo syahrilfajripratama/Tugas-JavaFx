da3f38b8e4c574e216651caa6ba4b19b2140beab
syahrilfajripratama <83504513+syahrilfajripratama@users.noreply.github.com>
2021-12-05 14:22:01 +0700

    First

---
+++ form.java
@@ -0,0 +1,9 @@
+import javafx.*;
+
+public class Main {
+    Stage stage = new Stage();
+    Root root = new Root();
+    Scene scene = new Scene(root);
+    stage.setScene(scene);
+    stage.show();
+}
